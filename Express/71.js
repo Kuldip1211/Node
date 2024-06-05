dfab0fba086b06d329da97ce0f4a3f23bf133258
kanbha@1211 <162014414+Kuldip1211@users.noreply.github.com>
2024-06-06 00:32:40 +0530

    Update 71.js

--- Express/71.js
+++ Express/71.js
@@ -1,1 +1,16 @@
+const express = require('express');
 
+const app = express();
+
+app.get('/', (req, res) => {
+ res.end("hello kanbha")
+})
+
+app.get('/msg', (req, res) => {
+ res.end("<h1>hello kanbha</h1><br><h2>Wel come to the server</h2>")
+ 
+})
+
+app.listen(3000, ()=>{
+ console.log('listening on 3000');
+});
