11c01f1baf6793813502d5a584c1c853dd6c2a70
kanbha@1211 <162014414+Kuldip1211@users.noreply.github.com>
2024-06-06 01:04:08 +0530

    Update 72.js

--- Express/72.js
+++ Express/72.js
@@ -1,1 +1,58 @@
+const express = require('express');
 
+const app = express();
+
+const friends = [
+
+{
+ id : 1,
+ name : 'kanbha',
+},
+
+{
+ id : 2,
+ name : 'vimal',
+},
+
+{
+ id : 2,
+ name : 'mukesh',
+},
+
+{
+ id : 3,
+ name : 'kaha',
+},
+
+{
+ id : 5,
+ name : 'kanbha',
+}
+]
+
+app.get('/', (req, res) => {
+ res.end("hello kanbha")
+})
+
+app.get('/friends', (req, res) => {
+ res.json(friends);
+})
+
+app.get('/friends/:id', (req, res) => {
+ const friendid = Number(req.params.id);
+ const friend = friends[friendid];
+ if (friend){
+  res.json(friend);
+ }else{
+  res.end("<h1>404 NOT FOUND</h1>")
+ }
+})
+
+app.get('/msg', (req, res) => {
+ res.end("<h1>hello kanbha</h1><br><h2>Wel come to the server</h2>")
+ 
+})
+
+app.listen(3000, ()=>{
+ console.log('listening on 3000');
+});
